5cdac3ccd1c5153fbf67cb5386614157d5608740
naranjox <182925606+bravex999@users.noreply.github.com>
2025-12-07 18:51:10 +0100

    listo falta makefile

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
+#include <string>
 
-empezando....
+int main(){
+
+std::string _string = "HI THIS IS BRAIN";
+std::string* stringPTR = &_string;
+std::string& stringREF = _string;
+
+std::cout << &_string << std::endl;
+std::cout << stringPTR << std::endl;
+std::cout << &stringREF << std::endl;
+
+std::cout << _string << std::endl;
+std::cout << *stringPTR << std::endl;
+std::cout << stringREF << std::endl;
+return 0;
+}
+
