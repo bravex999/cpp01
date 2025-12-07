fcb4bd94db21099113b33caacd55e2c563a0d4eb
naranjox <182925606+bravex999@users.noreply.github.com>
2025-12-07 19:14:11 +0100

    listo

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,13 +7,14 @@ std::string _string = "HI THIS IS BRAIN";
 std::string* stringPTR = &_string;
 std::string& stringREF = _string;
 
-std::cout << &_string << std::endl;
-std::cout << stringPTR << std::endl;
-std::cout << &stringREF << std::endl;
+std::cout << "Address of string variable: " << &_string << std::endl;
+std::cout << "Address held by stringPTR: " << stringPTR << std::endl;
+std::cout << "Address held by stringREF: " << &stringREF << std::endl;
 
-std::cout << _string << std::endl;
-std::cout << *stringPTR << std::endl;
-std::cout << stringREF << std::endl;
+std::cout << "Value of string variable: " << _string << std::endl;
+std::cout << "Value pointed to be PTR: " << *stringPTR << std::endl;
+std::cout << "Value pointed to be REF: " << stringREF << std::endl;
 return 0;
 }
+
 
