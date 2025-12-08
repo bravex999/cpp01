2d680b31f4fd647d4ec973f3d7f21fd455391fcc
naranjox <182925606+bravex999@users.noreply.github.com>
2025-12-08 14:21:48 +0100

    listo ex03

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -1,12 +1,26 @@
+#include "Weapon.hpp"
 #include "HumanA.hpp"
 #include "HumanB.hpp"
-#include "Weapon.hpp"
+#include <iostream>
 
 int main()
 {
-    Weapon weapon("sword");
-
-    HumanA Chris = HumanA("Chris", weapon);
-    Chris.attack();
-    return 0;    
+    
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanA bob("Bob", club);
+        bob.attack();
+        club.setType("some other type of club");
+        bob.attack();
+    }
+    
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanB jim("Jim");
+        jim.setWeapon(club);
+        jim.attack();
+        club.setType("some other type of club");
+        jim.attack();
+    }
+    return 0;
 }
