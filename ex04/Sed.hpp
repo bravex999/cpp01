3d1e42e33d30d841b7460f28b2c20bbcb94d3209
naranjox <182925606+bravex999@users.noreply.github.com>
2025-12-09 15:47:09 +0100

    class Sed defined

--- ex04/Sed.hpp
+++ ex04/Sed.hpp
@@ -1,6 +1,18 @@
 #ifndef SED_HPP
-#define SED_HPP
+# define SED_HPP
+#include <iostream>
+#include <string>
 
 class Sed
 {
-}
+    private:
+        std::string _file;
+    public:
+        Sed(std::string file);
+        ~Sed();
+
+        void replaceWord(std::string s1, std::string s2);
+    
+
+};
+#endif
