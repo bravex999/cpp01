1bfefb50f70dba4422fdf4843193b6a104d4d105
naranjox <182925606+bravex999@users.noreply.github.com>
2025-12-10 13:26:37 +0100

    listo ex05

--- ex05/Harl.hpp
+++ ex05/Harl.hpp
@@ -1,2 +1,25 @@
 #ifndef HARL_HPP
-#define HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+# include <string>
+
+class Harl
+{
+private:
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
+    
+    
+    void (Harl::*_ptrFuncs[4])(void);
+
+public:
+    Harl(void);
+    ~Harl(void);
+
+    void complain(std::string level);
+};
+
+#endif
