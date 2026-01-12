e6202a39d94036d865eb44abdd144227a7ee09b1
bravex999 <182925606+bravex999@users.noreply.github.com>
2026-01-12 11:57:09 +0100

    validacion incial de N y name

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -2,6 +2,8 @@
 
 Zombie* zombieHorde( int N, std::string name )
 {
+    if(N <= 0 || name.empty())
+        return NULL;
     int i;
     Zombie* _horde = new Zombie[N];
     for(i = 0; i < N; i++)
